44e79541896620d7ce6351052c843a2ae5d96920
edoardovincenzi <edoardo.vincenzi95@gmail.com>
2023-04-20 21:33:33 +0200

    adding react, and create one component for testing

---
+++ src/components/TestReact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TestReact = () => {
+  return (
+    <div>
+      <button onClick={() => console.log('Clicked')}>
+        Press me for console log
+      </button>
+    </div>
+  );
+};
+
+export default TestReact;
